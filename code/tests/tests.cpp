8852dec326721410ec93d1aa3e4f51a66ecde17f
Kavya Puranam <80189246+kavpurana@users.noreply.github.com>
2023-10-31 21:17:39 -0500

    Update tests.cpp

--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -23,3 +23,22 @@ TEST_CASE("Smallest - PageRank test for 5 elements", "[weight=5]")
   REQUIRE(sum < 100.1); 
   REQUIRE(sum > 99.9);
 }
+
+TEST_CASE("Second Smallest - PageRank test for 25 elements", "[weight=5]")
+{
+  std::vector<int> top3 = {18, 5, 4};
+  int lowest = 7;
+  std::vector<std::vector<int>> adjacency_matrix = csvToAdjacency("../../data/second_smallest.csv");
+  std::vector<float> out = pageRankAlgorithm(adjacency_matrix, 100, 0.85);
+  std::vector<float> three_largest_ranks = getTopThree(out);
+  REQUIRE(three_largest_ranks.at(0) == out.at(18));
+  REQUIRE(three_largest_ranks.at(1) == out.at(5));
+  REQUIRE(three_largest_ranks.at(2) == out.at(4));
+  float sum = 0;
+  for (float x : out) {
+    sum += x;
+  }
+  REQUIRE(sum < 100.1); 
+  REQUIRE(sum > 99.9);
+}
+
