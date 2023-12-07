e36f07aea11db20c6cdd8f729083382eafade7b2
Hrishi Shah <112705926+Hrishis2@users.noreply.github.com>
2023-12-06 23:05:36 -0600

    Update main.cpp

--- code/entry/main.cpp
+++ code/entry/main.cpp
@@ -1,4 +1,10 @@
 #include "../src/pagerank.h"
+#include <iostream>
+#include <vector>
 int main() {
-    return 0;
+    std::vector<std::vector<int>> edge_list = csvToEdgeList("../../data/largest.csv");
+    std::vector<float> out = pageRankAlgorithm(edge_list, 62500, 0.85); //passing in adjacency matrix, 62500 iterations, and damping factor of 0.85
+    for (size_t i = 0; i < out.size(); i++) {
+        std::cout << "Page " << i << " has rank " << out.at(i) << std::endl;
+    }
 }
